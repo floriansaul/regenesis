612b4bdea62f4490fe633c38b75ed9a967138d25
Florian <florian@notion.ai>
2024-01-12 15:44:27 -0500

    fix linter that I apparently have one of

--- src/app/_components/game.tsx
+++ src/app/_components/game.tsx
@@ -7,10 +7,10 @@ import { Tile } from "./tile";
 const GRIDSIZE = 5;
 
 export function Game() {
-  let g:{  [key: string]: string;} = {};
+  const g: Record<string, string> = {};
   for (let r = 0; r < GRIDSIZE; r++) {
     for (let c = 0; c < GRIDSIZE; c++) {
-      g[`${r}-${c}`] = 'blue';
+      g[`${r}-${c}`] = "blue";
     }
   }
   const [grid, setGrid] = useState(g);
@@ -18,13 +18,17 @@ export function Game() {
   const handleTileClick = (row: number, col: number) => {
     console.log(row, col);
 
-    let updatedGrid = {
+    const updatedGrid = {
       ...grid,
-      [`${row}-${col}`]: grid[`${row}-${col}`] == 'blue' ?  'red' : 'blue',
-      [`${row+1}-${col}`]: grid[`${row+1}-${col}`] == 'blue' ?  'red' : 'blue',
-      [`${row-1}-${col}`]: grid[`${row-1}-${col}`] == 'blue' ?  'red' : 'blue',
-      [`${row}-${col+1}`]: grid[`${row}-${col+1}`] == 'blue' ?  'red' : 'blue',
-      [`${row}-${col-1}`]: grid[`${row}-${col-1}`] == 'blue' ?  'red' : 'blue',
+      [`${row}-${col}`]: grid[`${row}-${col}`] == "blue" ? "red" : "blue",
+      [`${row + 1}-${col}`]:
+        grid[`${row + 1}-${col}`] == "blue" ? "red" : "blue",
+      [`${row - 1}-${col}`]:
+        grid[`${row - 1}-${col}`] == "blue" ? "red" : "blue",
+      [`${row}-${col + 1}`]:
+        grid[`${row}-${col + 1}`] == "blue" ? "red" : "blue",
+      [`${row}-${col - 1}`]:
+        grid[`${row}-${col - 1}`] == "blue" ? "red" : "blue",
     };
     setGrid(updatedGrid);
   };
@@ -33,19 +37,23 @@ export function Game() {
 
   return (
     <div>
-      {Array(GRIDSIZE).fill(0).map((_, r) => (
-        <div key={r}>
-          {Array(GRIDSIZE).fill(0).map((_, c) => (
-            <Tile
-              key={`${r}-${c}`}
-              color={grid[`${r}-${c}`] ?? 'purple'}
-              handleTileClick={() => {
-                handleTileClick(r, c);
-              }}
-            />
-          ))}
-        </div>
-      ))}
+      {Array(GRIDSIZE)
+        .fill(0)
+        .map((_, r) => (
+          <div key={r}>
+            {Array(GRIDSIZE)
+              .fill(0)
+              .map((_, c) => (
+                <Tile
+                  key={`${r}-${c}`}
+                  color={grid[`${r}-${c}`] ?? "purple"}
+                  handleTileClick={() => {
+                    handleTileClick(r, c);
+                  }}
+                />
+              ))}
+          </div>
+        ))}
     </div>
   );
 }
