c4f4f24427a2ac6087c3d2300ba59ce2ff1b3478
Florian <florian@notion.ai>
2024-01-18 11:20:16 -0500

    mobile size fix

--- src/app/_components/tile.tsx
+++ src/app/_components/tile.tsx
@@ -1,25 +1,21 @@
 "use client";
 
-export function Tile(props:{handleTileClick:()=>void, color:string}) {
-
+export function Tile(props: { handleTileClick: () => void; color: string }) {
   // console.log('rendering tile!');
 
   let css;
-  if (props.color == 'red')
-  {
+  if (props.color == "red") {
     css = "bg-red-500/60 hover:bg-red-500/80";
-  }
-
-  else
-  {
+  } else {
     css = "bg-sky-500/60 hover:bg-sky-500/80";
   }
   return (
-      <button
-        className={`rounded-full px-10 py-10 font-semibold transition {} ${css}`}
-        onClick={props.handleTileClick}
-      >
-
-      </button>
+    <button
+      type="button"
+      className={`{} rounded-full px-6 py-6 sm:px-10 sm:py-10 font-semibold transition ${css}`}
+      onClick={props.handleTileClick}
+    >
+      
+    </button>
   );
 }
